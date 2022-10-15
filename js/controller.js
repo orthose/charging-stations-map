6ef07e8dbd5b56b877c772587989875e07a89f60
maxime vincent <maxime.vincent1@u-psud.fr>
2022-10-15 12:11:49 +0200

    Contrôleur pour choisir la réprésentation à afficher

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,7 @@
 let map;
+let mainSketch = null;
+let legendSketch = null;
+
 window.onload = () => {
     mapboxgl.accessToken = config.mapboxToken;
     map = new mapboxgl.Map({
@@ -11,8 +14,32 @@ window.onload = () => {
     map.addControl(new mapboxgl.NavigationControl());
     map.on('style.load', () => {
         map.setFog({}); // Set the default atmosphere style
-        //new p5(sketchCircle, "data");
-        new p5(sketchLocation, "data");
-        //new p5(sketchLegendCircle, "legend");
+
+        // Par défaut représentation de la localisation des bornes
+        document.getElementById("button-location").className = "selected";
+        mainSketch = new p5(sketchLocation, "data");
+        
+        // Contrôleur pour l'interaction utilisateur
+        document.getElementById("controller").addEventListener("click", function(event) {
+            const currentId = document.getElementsByClassName("selected")[0].id;
+            if (event.target.tagName === "BUTTON" && event.target.id !== currentId) {
+                // Libération de la représentation précédente
+                document.getElementsByClassName("selected")[0].className = "";
+                document.getElementById(event.target.id).className = "selected";
+                if (mainSketch !== null) mainSketch.remove();
+                if (legendSketch !== null) legendSketch.remove();
+                document.getElementById("legend").innerHTML = "";
+                
+                // Changement de représentation
+                if (event.target.id === "button-location") {
+                    mainSketch = new p5(sketchLocation, "data");
+                    legendSketch = null;
+                }
+                else if (event.target.id === "button-circle") {
+                    mainSketch = new p5(sketchCircle, "data");
+                    legendSketch = new p5(sketchLegendCircle, "legend");
+                }
+            }
+        });
     });
 }
