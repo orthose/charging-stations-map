8c5c38bec52e7f6b48a6b3dcfbbaf4938c754ff1
maxime vincent <maxime.vincent1@u-psud.fr>
2022-10-16 17:15:52 +0200

    Suppression du KNN implémenté en Python tout est fait côté Javascript

--- config-template.js
+++ config-template.js
@@ -2,6 +2,5 @@ const config = {
     // Créer un compte sur https://account.mapbox.com
     mapboxToken: "",
     // Chemin vers les fichiers CSV
-    dataPathRawTable: "data/bornes-recharge.csv",
-    dataPathKNNTable: "data/bornes-voisines.csv",
+    dataPath: "data/bornes-recharge.csv",
 }
