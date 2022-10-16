e9905b2e95a0c4ca2de86dd69b6401a8489fbb5d
maxime vincent <maxime.vincent1@u-psud.fr>
2022-10-16 19:40:32 +0200

    Affichage de la localisation des bornes si niveau de zoom suffisant car agrégation devient impossible

--- js/controller.js
+++ js/controller.js
@@ -37,21 +37,35 @@ window.onload = () => {
 
         // Si on zoom il faut recalculer les clusters pour l'agrégation
         let lastZoom = 5;
-        map.on("zoom", () => {
+        let sketchLocationIsLoaded = false;
+
+        function zoomCallback(enforce=false) {
+            const sketchAggIsSelected = document.getElementById("button-agg").className === "selected";
             let currentZoom = map.getZoom();
             // Calcul des clusters par rapport au zoom avec un pas de 1
-            if (lastZoom+1 <= currentZoom || currentZoom <= lastZoom) {
+            if ((sketchAggIsSelected && (lastZoom+1 <= currentZoom || currentZoom <= lastZoom)) || enforce) {
+                // On a suffisamment dézoomé pour afficher l'agrégation
+                if (currentZoom <= lastZoom && lastZoom >= 8) {
+                    sketchLocationIsLoaded = false;
+                    mainSketch.remove();
+                    mainSketch = new p5(sketchAgg, "data");
+                }
                 currentZoom = Math.floor(currentZoom);
                 let distMax = 100_000;
                 if (currentZoom === 5) distMax = 100_000;
                 else if (currentZoom === 6) distMax = 50_000;
                 else if (currentZoom === 7) distMax = 30_000;
                 // Afficher la localisation des bornes au-delà du niveau de zoom 8
-                //else if (currentZoom >= 8) {}
+                else if (currentZoom >= 8 && !sketchLocationIsLoaded) {
+                    sketchLocationIsLoaded = true;
+                    mainSketch.remove();
+                    mainSketch = new p5(sketchLocation, "data");
+                }
                 if (currentZoom < 8) computeClusters(distMax=distMax);
                 lastZoom = currentZoom;
-            } 
-        });
+            }
+        }
+        map.on("zoom", zoomCallback);
         
         // Contrôleur pour l'interaction utilisateur
         document.getElementById("visu-choice").addEventListener("click", function(event) {
@@ -75,6 +89,8 @@ window.onload = () => {
                 }
                 else if (event.target.id === "button-agg") {
                     mainSketch = new p5(sketchAgg, "data");
+                    sketchLocationIsLoaded = false;
+                    zoomCallback(enforce=true);
                     legendSketch = null;
                 }
             }
