e94e0a561c49c2f5fbe1031522145a85a904f349
maxime vincent <maxime.vincent1@u-psud.fr>
2022-10-16 17:16:43 +0200

    Sketch de visualisation de l'agrégation

--- js/controller.js
+++ js/controller.js
@@ -2,22 +2,43 @@ let map;
 let mainSketch = null;
 let legendSketch = null;
 
+// Montluçon comme ville de référence car au centre de la France
+const referenceTown = [2.6 , 46.333328];
+
+// Échelle en mètres par pixel selon la latitude et le niveau de zoom
+// https://docs.mapbox.com/help/glossary/zoom-level/#zoom-levels-and-geographical-distance
+// https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Resolution_and_Scale
+// https://wiki.openstreetmap.org/wiki/Zoom_levels
+function scaleZoomLevel(zoomLevel, latitude=referenceTown[1]) {
+    return 156543.03 * Math.cos(radians(latitude)) / Math.pow(2, zoomLevel+1);
+}
+
 window.onload = () => {
     mapboxgl.accessToken = config.mapboxToken;
     map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [2.6 , 46.333328], // [lng, lat]
+        center: referenceTown, // [lng, lat]
         zoom: 5,
         projection: 'globe'
     });
     map.addControl(new mapboxgl.NavigationControl());
+    const scale = new mapboxgl.ScaleControl({
+        maxWidth: 80,
+        unit: 'metric'
+    });
+    map.addControl(scale);
     map.on('style.load', () => {
         map.setFog({}); // Set the default atmosphere style
 
         // Par défaut représentation de la localisation des bornes
         document.getElementById("button-location").className = "selected";
         mainSketch = new p5(sketchLocation, "data");
+
+        // Si on zoom il faut recalculer les clusters pour l'agrégation
+        map.on("zoom", () => {
+            console.log(map.getZoom());
+        });
         
         // Contrôleur pour l'interaction utilisateur
         document.getElementById("controller").addEventListener("click", function(event) {
--- js/sketch-agg.js
+++ js/sketch-agg.js
@@ -15,11 +15,16 @@ function sketchAgg(p) {
         // Affichage des bornes de recharge
         for (let cluster of data.clusters) {
             // Projection de [longitude, latitude] vers les pixels de l'écran
-            const point = map.project([cluster.longitude, cluster.latitude]);
+            const point = map.project([cluster.lon, cluster.lat]);
             const px = point.x; const py = point.y;
-            p.stroke(0);
-            p.circle(px, py, 5);
-            // Afficher somme mettre 100 km de distance
+            p.noStroke(); p.fill(49, 163, 84);
+            p.circle(px, py, 25);
+            p.fill(255);
+            p.textAlign(p.CENTER, p.CENTER);
+            p.textSize(9);
+            p.text(cluster.sum <= 999 ? cluster.sum : "999+", px, py);
+            p.stroke(0); p.noFill();
+            p.circle(px, py, 2 * (cluster.distAvg / scaleZoomLevel(map.getZoom())));
         }
     }
 }
@@ -37,44 +42,5 @@ function sketchAgg(p) {
     }
     
     p.draw = function () {
-        p.stroke(0); p.line(0, 0, p.width, 0);
-
-        // Puissance
-        p.stroke(0); p.fill(0); p.textSize(20); p.textAlign(p.LEFT, p.TOP);
-        p.text("PUISSANCE", 0.10 * p.width, 0.05 * p.height);
-
-        p.textAlign(p.LEFT, p.CENTER);
-        let legend = [["≤ 22 kW", "#74c47663"], ["≤ 50 kW", "#31a354"], ["> 50 kW", "#006d2c"]];
-        let offset = 1.5;
-        for (let [text, color] of legend) {
-            p.noStroke(); p.fill(color);
-            p.rect(0.10 * p.width, offset * 0.10 * p.height, 0.10 * p.width, 0.10 * p.height);
-            p.stroke(0); p.fill(0); 
-            p.text(text, 3 * 0.10 * p.width, (offset + 0.5) * 0.10 * p.height);
-            offset += 1;
-        }
-
-        // Nombre de prises
-        p.stroke(0); p.fill(0); p.textSize(20); p.textAlign(p.LEFT, p.TOP);
-        p.text("NOMBRE DE PRISES", 0.10 * p.width, 5 * 0.10 * p.height);
-
-        p.textAlign(p.LEFT, p.CENTER);
-        legend = [["≤ 2", 4.0], ["≤ 4", 8.0], ["≤ 8", 12.0]];
-        offset = 0;
-        for (let [text, radius] of legend) {
-            p.stroke(0); p.strokeWeight(1); p.noFill();
-            p.circle(0.15 * p.width, (6.25 + offset) * 0.10 * p.height, radius);
-            p.fill(0); p.text(text, 3 * 0.10 * p.width, (6.25 + offset) * 0.10 * p.height);
-            offset += 1;
-        }
-        legend = [["≤ 16", 16.0], ["> 16", 20.0]];
-        offset = 0;
-        for (let [text, radius] of legend) {
-            p.stroke(0); p.strokeWeight(1); p.noFill();
-            p.circle(0.55 * p.width, (6.25 + offset) * 0.10 * p.height, radius);
-            p.fill(0);
-            p.text(text, 0.70 * p.width, (6.25 + offset) * 0.10 * p.height);
-            offset += 1;
-        }
     }
 }*/
